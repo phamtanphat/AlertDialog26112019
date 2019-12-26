73c3a30413d309615051216a19e8d84598c8cda7
phatdroid94 <phatdroid94@gmail.com>
2019-12-26 18:04:38 +0700

    init

---
+++ app/src/main/java/com/example/alertdialog26112019/FastFood.java
@@ -0,0 +1,9 @@
+package com.example.alertdialog26112019;
+
+public class FastFood {
+   private String sandwich;
+   private String mexican;
+   private String burger;
+   private String coffee;
+   private String frenchfires;
+}
